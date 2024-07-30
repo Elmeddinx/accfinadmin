4aeb916fcbf05826d736f2e986a63d3a1e4c2627
Elməddin V. Xudaverdiyev <XudaverdiyevE@atb.local>
2024-07-30 16:45:17 +0400

    create home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-function togglePassword() {
-    const passwordInput = document.getElementById('passwordInput');
-    const passwordToggle = document.querySelector('.toggle-password img');
-    
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        passwordToggle.src = './assets/icons/eye.png'; 
+if (document.body.id == "login-page") {
+  function togglePassword() {
+    const passwordInput = document.getElementById("passwordInput");
+    const passwordToggle = document.querySelector(".toggle-password img");
+
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      passwordToggle.src = "./assets/icons/eye.png";
     } else {
-        passwordInput.type = 'password';
-        passwordToggle.src = './assets/icons/eye-hide.png'; 
+      passwordInput.type = "password";
+      passwordToggle.src = "./assets/icons/eye-hide.png";
     }
+  }
 }
